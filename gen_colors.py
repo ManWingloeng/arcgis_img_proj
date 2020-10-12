6f8c418a0992ed24065234b66474dfac9d3546ec
Wen YongLiang <strawsuper@gmail.com>
2020-10-12 19:22:29 +0800

    add 4x4c

--- gen_colors.py
+++ gen_colors.py
@@ -3,13 +3,14 @@ import os
 import numpy as np
 
 save_path = 'colors_png/'
+os.makedirs(save_path, exist_ok=True)
 colors = [i for i in range(255)]
 c_list = []
 width = 4
 for c in colors:
-    c_png = str(c) + "_4x4.png"
+    c_png = str(c) + "_4x4_c.png"
     c_list.append(c_png)
-    img = np.zeros((width, width), np.uint8)*c
+    img = np.ones((width, width), np.uint8)*c
     c_path = os.path.join(save_path, c_png)
     # cv2.circle(img, (width // 2, width // 2), width // 2, c, -1)
     img_bgr = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
